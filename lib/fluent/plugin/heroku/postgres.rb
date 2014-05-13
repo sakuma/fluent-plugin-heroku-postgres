dce73cbfb2514dba2cdcc5e897e8189e4e48496e
Naohiro Sakuma <n.sakuma@everyleaf.com>
2014-05-13 15:22:00 +0900

    bootup gem template

---
+++ lib/fluent/plugin/heroku/postgres.rb
@@ -0,0 +1,11 @@
+require "fluent/plugin/heroku/postgres/version"
+
+module Fluent
+  module Plugin
+    module Heroku
+      module Postgres
+        # Your code goes here...
+      end
+    end
+  end
+end
